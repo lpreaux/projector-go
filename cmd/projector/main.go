b493ed04c77c41d5a6993bdcb4b880c41e970596
lpreaux <lpreaux@diginamic-formation.fr>
2024-02-21 15:15:03 +0100

    feat(config): Handling config and args verification

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -12,5 +12,11 @@ func main() {
 		log.Fatalf("unable to get options %v", err)
 	}
 
+	config, err := cli.NewConfig(opts)
+	if err != nil {
+		log.Fatalf("unable to get config %v", err)
+	}
+
 	fmt.Printf("opts: %+v", opts)
+	fmt.Printf("config: %+v", config)
 }
